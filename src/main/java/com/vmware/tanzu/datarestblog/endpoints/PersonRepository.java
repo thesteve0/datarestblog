aacd5e4294aa25b8e319cc1ab2c9092d3a2aa1a2
Steven Pousty <steve.pousty@gmail.com>
2023-03-27 15:48:37 -0700

    finishing the PersonRepository

--- src/main/java/com/vmware/tanzu/datarestblog/endpoints/PersonRepository.java
+++ src/main/java/com/vmware/tanzu/datarestblog/endpoints/PersonRepository.java
@@ -1,10 +1,21 @@
 package com.vmware.tanzu.datarestblog.endpoints;
 
 import com.vmware.tanzu.datarestblog.dataobjects.Person;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "people", path = "people")
 public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+
+    Person findById(Long id);
+
+    @Query("SELECT p FROM Person p WHERE p.lastName = :lastName")
+    List<Person> findByLastName(@Param("lastName") String lastName);
+
+
 }
 
