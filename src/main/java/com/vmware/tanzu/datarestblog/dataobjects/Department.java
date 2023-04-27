cc60c80b21c4003ce9479d1f91e25b8744ff4017
Steven Pousty <steve.pousty@gmail.com>
2023-04-27 14:19:42 -0700

    updated to Lombok based on feedback from Josh Long and Jakub Pilimon. Damn that was easy

--- src/main/java/com/vmware/tanzu/datarestblog/dataobjects/Department.java
+++ src/main/java/com/vmware/tanzu/datarestblog/dataobjects/Department.java
@@ -5,9 +5,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 @Entity
+@Data
+@NoArgsConstructor
 public class Department {
 
     @Id
@@ -19,27 +24,4 @@ public class Department {
     @OneToMany(mappedBy = "department")
     private List<Person> people;
 
-    public Long getId() {
-        return id;
     }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<Person> getPeople() {
-        return people;
-    }
-
-    public void setPeople(List<Person> people) {
-        this.people = people;
-    }
-}
--- src/main/java/com/vmware/tanzu/datarestblog/dataobjects/Person.java
+++ src/main/java/com/vmware/tanzu/datarestblog/dataobjects/Person.java
@@ -5,8 +5,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
 public class Person {
 
     @Id
@@ -20,35 +24,5 @@ public class Person {
     @ManyToOne
     private Department department;
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Department getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(Department department) {
-        this.department = department;
-    }
 }
